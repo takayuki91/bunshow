24af019641ebdfee3e34b4aa3f2a9b60d68a603f
takayuki91 <takayuki.nakatsuji@gmail.com>
2023-06-06 09:31:33 +0000

    [Add]post newpartial&index

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,15 +1,33 @@
 class Public::PostsController < ApplicationController
   
   def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if  @item.save
+      flash[:success] = "あなたのbunshowを共有しました!!"
+      redirect_to posts_path(@post)
+    else
+      flash.now[:danger] = "bunshowを入力してください"
+      render :index
+    end
   end
   
   def index
+    @posts = Post.all
   end
   
   def show
   end
   
   def edit
+  end
+  
+  def post_params
+    params.require(:post).permit(:post_images, :explanation)
   end
     
 
