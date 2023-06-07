2c891ca16cee0f42c9385a106ba11b7a0eba287e
takayuki91 <takayuki.nakatsuji@gmail.com>
2023-06-07 13:50:37 +0000

    [Add]favorite likes&paragons&bookmarks

--- app/controllers/public/paragons_controller.rb
+++ app/controllers/public/paragons_controller.rb
@@ -1,2 +1,19 @@
 class Public::ParagonsController < ApplicationController
+  
+  before_action :authenticate_user!
+  
+  def create
+    @post = Post.find(params[:post_id])
+    paragon = current_user.paragons.new(post_id: @post.id)
+    paragon.save
+    redirect_to request.referer
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    paragon = current_user.paragons.find_by(post_id: @post.id)
+    paragon.destroy
+    redirect_to request.referer
+  end
+  
 end
