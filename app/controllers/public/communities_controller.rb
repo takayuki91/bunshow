b0593627f9d83a4f51a1e675d7ab54a7fbc18771
takayuki91 <takayuki.nakatsuji@gmail.com>
2023-06-11 08:34:46 +0000

    [Add]community create&edit

--- app/controllers/public/communities_controller.rb
+++ app/controllers/public/communities_controller.rb
@@ -5,10 +5,28 @@ class Public::CommunitiesController < ApplicationController
   end
   
   def join
+    @user = current_user
     @community = Community.find(params[:id])
     current_user.communities << @community
     flash[:dark] = "コミュニティに参加しました!!"
-    redirect_to communities_path
+    redirect_to user_path(@user.id)
+  end
+  
+  def edit
+    @user = current_user
+    @communities = Community.all
+  end
+
+  def update
+    @user = current_user
+    @user.community_ids = params[:user][:community_ids] # 選択されたコミュニティのIDを更新
+    if @user.save
+      flash[:dark] = "コミュニティを更新しました!!"
+      redirect_to user_path(@user.id)
+    else
+      flash.now[:alert] = "コミュニティの更新に失敗しました。"
+      render :edit
+    end
   end
   
 end
