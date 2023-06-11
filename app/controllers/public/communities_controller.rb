aa9deb34460f8a777faab270e893404ed2f50229
takayuki91 <takayuki.nakatsuji@gmail.com>
2023-06-11 07:29:11 +0000

    [Add]community

--- app/controllers/public/communities_controller.rb
+++ app/controllers/public/communities_controller.rb
@@ -1,2 +1,14 @@
 class Public::CommunitiesController < ApplicationController
+  
+  def index
+    @communities = Community.all
+  end
+  
+  def join
+    @community = Community.find(params[:id])
+    current_user.communities << @community
+    flash[:dark] = "コミュニティに参加しました!!"
+    redirect_to communities_path
+  end
+  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,12 +3,28 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
 
-# 管理者アカウント
+管理者アカウント
 Admin.create!(
   email: "admin@example.com",
   password: "hogehoge",
   password_confirmation: "hogehoge"
 )
+
+leader = Community.create(name: "指導者")
+teacher, coach = leader.children.create(
+  [
+    { name: "教員" },
+    { name: "コーチ" },
+  ]
+)
+
+["幼稚園・保育園", "小学校", "中学校", "高等学校", "大学・大学院", "専門学校", "他のカテゴリ"].each do |name|
+  teacher.children.create(name: name)
+end
+
+["スポーツ", "ビジネス", "ライフ", "カウンセリング", "他のカテゴリ"].each do |name|
+  coach.children.create(name: name)
+end
