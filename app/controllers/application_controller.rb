571637b00ea1aaa34d4c3241929b8fd236cc228a
takayuki91 <takayuki.nakatsuji@gmail.com>
2023-06-13 06:58:55 +0000

    [Add]layout color

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_admin!, only: [:guest_sign_in], if: :admin_url
 
   # フラッシュメッセージの色指定
   add_flash_types :success, :info, :warning, :danger, :light, :dark
+
+  # 管理者のurlを直接入力した際にログイン画面に
+  def admin_url
+    request.fullpath.include?("/admin")
+  end
 
   protected
 
