80fb697d4576813cc0fd31c4dd91ab0c5e76d812
takayuki91 <takayuki.nakatsuji@gmail.com>
2023-06-17 04:25:42 +0000

    [Add]layout background-color

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,7 +11,10 @@ import "channels"
 import "jquery";
 import "popper.js";
 import "bootstrap";
-import "../stylesheets/application"; 
+
+import "../stylesheets/application";
+import "../stylesheets/new_form";
+import "../stylesheets/card";
 
 Rails.start()
 Turbolinks.start()
